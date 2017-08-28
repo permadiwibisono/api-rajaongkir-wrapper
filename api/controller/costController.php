e8c9ba12184be4901468bd3dbf086b7cddbb5800
Permadi Wibisono <mbapewe@gmail.com>
2017-08-28 19:30:58 +0700

    Modify cost routing, service field is optional, origin and destination can be integer

--- api/controller/costController.php
+++ api/controller/costController.php
@@ -1,8 +1,8 @@
 <?php
-	//require_once('\model\ResultWrapper.php');
-	require_once(__ROOT__.'\model\Cost.php');
-    require_once(__ROOT__.'\controller\cityController.php');
-    require_once(__ROOT__.'\controller\AbstractAPI.php');
+    //require_once('\model\ResultWrapper.php');
+    require_once(__ROOT__.DS.'model'.DS.'Cost.php');
+    require_once(__ROOT__.DS.'controller'.DS.'cityController.php');
+    require_once(__ROOT__.DS.'controller'.DS.'AbstractAPI.php');
 class costController extends AbstractAPI{
     private $urlApi='';
     //private $result;
@@ -10,26 +10,31 @@ class costController extends AbstractAPI{
     }
 
     public function Process(){
-    	switch($this->method)
-    	{
-    		case "POST":$result=$this->Post();break;
-    		default:{
-                $result=$this->_response(500);
-    		} break;
-    	}
-    	return $result;
+        switch($this->method)
+        {
+            case "POST":$result=$this->Post();break;
+            default:{
+                $result=$this->_response(500,'Method Not Allowed!');
+            } break;
+        }
+        return $result;
     }
-    public function Post(){    	
-		$origin=457; //origin city id
-		$destination=$this->param["destination"];
-		$weight=$this->param["weight"];
-		$courier=$this->param["courier"];
-		$city=$this->GetCity($destination);
+    public function Post(){     
+        $origin=$this->param["origin"];
+        $destination=$this->param["destination"];
+        $weight=$this->param["weight"];
+        $courier=$this->param["courier"];
+        $originCity=is_numeric($origin)?$origin:$this->GetCity($origin);
+        $city=is_numeric($destination)?$destination:$this->GetCity($destination);
+        if(is_null($originCity))
+        {
+            return $result=$this->_response(404,'Origin not Found!');
+        }
         if(is_null($city))
         {
-            return $result=$this->_response(500);
+            return $result=$this->_response(500,'Origin not Found!');
         }
-        $cost=$this->Cost($city,$origin,$weight,$courier);
+        $cost=$this->Cost($city,$originCity,$weight,$courier);
         return $cost;
     }
     function Cost($city,$origin,$weight,$courier)
@@ -44,7 +49,7 @@ class costController extends AbstractAPI{
           CURLOPT_TIMEOUT => 30,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => $this->method,
-          CURLOPT_POSTFIELDS => "origin=". $origin ."&destination=" .$city->id. "&weight=".$weight."&courier=".$courier,
+          CURLOPT_POSTFIELDS => "origin=". (is_numeric($origin)?$origin:$origin->id) ."&destination=" .(is_numeric($city)?$city:$city->id). "&weight=".$weight."&courier=".$courier,
           CURLOPT_HTTPHEADER => array(
             "content-type: application/x-www-form-urlencoded",
             "key: ".$this->key
@@ -60,10 +65,14 @@ class costController extends AbstractAPI{
         else{
             $obj=json_decode($response2);
             $result=$this->_response($obj->rajaongkir->status->code,$obj->rajaongkir->status->description);
-            $rList=$obj->rajaongkir->results[0]->costs;
             if ($obj->rajaongkir->status->description=="OK") 
             {
-                 
+                $availableService=['REG','YES','OKE','EKO','CTC','CTCYES','ECO','ONS','SDS','Paket Kilat Khusus','Express Next Day Barang','Express Sameday Barang'];
+                $rList=array();
+                foreach ($obj->rajaongkir->results[0]->costs as $cost) {
+                    if(in_array($cost->service,$availableService))
+                        array_push($rList,$cost);
+                }
                 /*$list=array();              
                 for($i=0;$i<count($rList);$i++)
                 {
@@ -74,7 +83,7 @@ class costController extends AbstractAPI{
                     array_push($list,$cost);
                 }*/
                 $cost=new Cost;
-                if(!is_null($this->param["service"]))
+                if(array_key_exists('service',$this->param))
                 {
                     $service=strtolower($this->param["service"]);
                     for ($i=0; $i < count($rList); $i++) { 
@@ -109,7 +118,10 @@ class costController extends AbstractAPI{
                 }
                 else
                 {
-                    $result=$this->_response(404,'Please provide Service!');
+                    $result->results=$obj->rajaongkir->results[0];
+                    $result->results->costs=$rList;
+                    return $result;
+                    // $result=$this->_response(404,'Please provide Service!');
                 }
                 if(is_null($cost->service))
                 {
@@ -121,21 +133,21 @@ class costController extends AbstractAPI{
         }
     }
     function GetCity($destination){
-    	$cityCtrl=new cityController();
-    	$cityCtrl->key=$this->key;
-    	$cityCtrl->method='GET';
+        $cityCtrl=new cityController();
+        $cityCtrl->key=$this->key;
+        $cityCtrl->method='GET';
         $cityCtrl->basicUrl=$this->basicUrl;
-    	$cityList=$cityCtrl->Get();
+        $cityList=$cityCtrl->Get();
         $city=null;
-		for($i=0;$i<count($cityList->results);$i++)
-		{
-			if(strtolower($cityList->results[$i]->name)==strtolower($destination))
-			{
-				$city=$cityList->results[$i];
-				break;
-			}
-		}
-    	return $city;
+        for($i=0;$i<count($cityList->results);$i++)
+        {
+            if(strtolower($cityList->results[$i]->name)==strtolower($destination))
+            {
+                $city=$cityList->results[$i];
+                break;
+            }
+        }
+        return $city;
     }
 }
 
